5095e702b97693a6fa125200291606745afdfae6
UDAY ALLU <udayallu2010@gmail.com>
2018-03-03 12:33:25 +0530

    Add files via upload

---
+++ DAT229x-Labfiles/graph/config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.endpoint = "uudaygraphdb.gremlin.cosmosdb.azure.com";
+config.primaryKey = "xqQcBWO5J7LcChL1xzBIji9aRGqseLBo0NyBLZiduwIz8Z45wkauF1aXqMlq8XvqS8AGH0rx36q0xuGYgvDjjw==";
+config.database = "GraphDB"
+config.collection = "EmployeeGraph"
+
+module.exports = config;
